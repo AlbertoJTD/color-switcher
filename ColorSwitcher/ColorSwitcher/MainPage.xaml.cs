31528d2bfe013829b1a8a22b133cfc8aa5478147
AlbertoJTD <albertojtd@outlook.com>
2023-07-03 19:45:17 -0600

    Add UI for hexadecimal code

--- ColorSwitcher/ColorSwitcher/MainPage.xaml.cs
+++ ColorSwitcher/ColorSwitcher/MainPage.xaml.cs
@@ -14,5 +14,26 @@ namespace ColorSwitcher
 		{
 			InitializeComponent();
 		}
+
+		private void ChangeColor(object sender, ValueChangedEventArgs e)
+		{
+			var red = redSlider.Value;
+			var green = greenSlider.Value;
+			var blue = blueSlider.Value;
+			Color bgColor = new Color(red, green, blue);
+
+			boxColor.BackgroundColor = bgColor;
+			labelHexColor.Text = ColorToHex(bgColor);
+        }
+
+		private string ColorToHex(Color bgColor)
+		{
+			int red = (int)(bgColor.R * 255);
+			int green = (int)(bgColor.G * 255);
+			int blue = (int)(bgColor.B * 255);
+			int alpha = (int)(bgColor.A * 255); // opacity
+
+			return $"#{red:X2}{green:X2}{blue:X2}{alpha:X2}"; // X2 -> Convert integer to hexadecimal code with 2 digits
+		}
 	}
 }
